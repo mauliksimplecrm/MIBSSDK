24cc5253e37b168d6909e992ac25806b5e906dbd
mauliksimplecrm <maulik.simplecrm@gmail.com>
2022-07-08 11:35:50 +0530

    First Commit

---
+++ MIBSSDK.swift
@@ -0,0 +1,8 @@
+//
+//  MIBSSDK.swift
+//  MIBSSDK
+//
+//  Created by Maulik Vora on 08/07/22.
+//
+
+import Foundation
---
+++ MIBSSDK/MIBSSDK.h
@@ -0,0 +1,18 @@
+//
+//  MIBSSDK.h
+//  MIBSSDK
+//
+//  Created by Maulik Vora on 08/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MIBSSDK.
+FOUNDATION_EXPORT double MIBSSDKVersionNumber;
+
+//! Project version string for MIBSSDK.
+FOUNDATION_EXPORT const unsigned char MIBSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIBSSDK/PublicHeader.h>
+
+
